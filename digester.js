7a071eb50161cc0dde3e3c5847253e5ce4291e5a
Matin Gühmann <guehmann@t-online.de>
2024-11-29 17:18:24 +0100

    Reduce gel size

--- digester.js
+++ digester.js
@@ -680,16 +680,16 @@ function getMaxLength2(fragments1, fragments2, fragments3)
 function plotFragments(fragments1, fragments2, fragments3, enzyme)
 {
 	const ladder      = [50, 100, 200, 300, 500, 1000, 1500, 2000, 3000, 5000]
-	const scaleFactor = 100;
+	const scaleFactor = 50;
 	let maxLadder = ladder[ladder.length-1];
 	let maxLength = getMaxLength2(fragments1, fragments2, fragments3);
 	if (maxLength < maxLadder)
 		maxLength = maxLadder;
 	maxLength = Math.round(Math.log(maxLength) * scaleFactor);
 
-	const padding     = 50;
-	const margin      = 50;
-	const bandLength  = 100;
+	const padding     = 25;
+	const margin      = 25;
+	const bandLength  = 50;
 	var gelCanvas = document.createElement('canvas');
 	gelCanvas.id = 'Gel-' + enzyme;
 	gelCanvas.width   = 4*bandLength + 5*margin;
