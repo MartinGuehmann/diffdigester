1e0205daea4eaf251c6bc4ecc8cd04aa35b110dc
Matin Gühmann <guehmann@t-online.de>
2024-12-01 17:44:54 +0100

    Add 10 bp band to ladder

--- digester.js
+++ digester.js
@@ -679,7 +679,7 @@ function getMaxLength2(fragments1, fragments2, fragments3)
 
 function plotFragments(fragments1, fragments2, fragments3, enzyme)
 {
-	const ladder      = [50, 100, 200, 300, 500, 1000, 1500, 2000, 3000, 5000]
+	const ladder      = [10, 50, 100, 200, 300, 500, 1000, 1500, 2000, 3000, 5000]
 	const scaleFactor = 50;
 	let maxLadder = ladder[ladder.length-1];
 	let maxLength = getMaxLength2(fragments1, fragments2, fragments3);
